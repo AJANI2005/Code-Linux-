16e4b490cbcd30bd27049dd6925e0ee7cb000695
AJANI2005 <ajanismall2005@gmail.com>
2025-06-07 14:08:01 -0400

    some leets :)

--- Testing/test.py
+++ Testing/test.py
@@ -1,8 +1,11 @@
-import math
-
-
 def factorial(n):
-    return math.factorial(n)
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1) 
+    
+
+
 
 
 print(factorial(5))
